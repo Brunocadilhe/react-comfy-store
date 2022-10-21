b99bd5d00330dc08f5162e063933569c76faa9b2
Bruno <cadilheb@gmail.com>
2022-10-21 12:02:50 +0200

    initial commit

---
+++ src/pages/AboutPage.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import styled from 'styled-components'
+import { PageHero } from '../components'
+import aboutImg from '../assets/hero-bcg.jpeg'
+
+const AboutPage = () => {
+  return (
+    <main>
+      <PageHero title='about' />
+      <Wrapper className='page section section-center'>
+        <img src={aboutImg} alt='nice desk' />
+        <article>
+          <div className='title'>
+            <h2>our story</h2>
+            <div className='underline'></div>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores,
+            deserunt voluptatem repellat nulla officiis provident accusantium ab
+            quia placeat esse, eveniet magnam iure omnis beatae ducimus,
+            pariatur quam aspernatur recusandae voluptas a facilis voluptatibus
+            assumenda eos. Libero facilis maxime earum dignissimos sapiente vel,
+            itaque, quibusdam cum delectus adipisci, dolore sint?
+          </p>
+        </article>
+      </Wrapper>
+    </main>
+  )
+}
+
+const Wrapper = styled.section`
+  display: grid;
+  gap: 4rem;
+  img {
+    width: 100%;
+    display: block;
+    border-radius: var(--radius);
+    height: 500px;
+    object-fit: cover;
+  }
+  p {
+    line-height: 2;
+    max-width: 45em;
+    margin: 0 auto;
+    margin-top: 2rem;
+    color: var(--clr-grey-5);
+  }
+  .title {
+    text-align: left;
+  }
+  .underline {
+    margin-left: 0;
+  }
+  @media (min-width: 992px) {
+    grid-template-columns: 1fr 1fr;
+  }
+`
+export default AboutPage
